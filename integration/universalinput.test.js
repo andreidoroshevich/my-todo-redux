1e612e2714045966c4066c931e1a656faff74cd5
a.n.darashevich <a.n.darashevich@gmail.com>
2022-05-18 15:26:36 +0300

    add snapshot-test

---
+++ integration/universalinput.test.js
@@ -0,0 +1,10 @@
+describe('UniversalInput', () => {
+    it('base example, visually looks correct', async () => {
+// APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=universal-input--input-story&args=&viewMode=story');
+        const image = await page.screenshot();
+// API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
